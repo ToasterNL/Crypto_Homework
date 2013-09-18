616978b8609743ff0985fbd0bc60c6c51956fe04
Aram Verstegen <aram.verstegen@gmail.com>
2013-09-18 16:53:20 +0200

    Optimized with MATH

--- week2/mac.py
+++ week2/mac.py
@@ -5,8 +5,7 @@ r=483754
 s=342534
 n = int(math.floor(math.log(m,p)))
 terms = [(i, m / p**i % p) for i in range(0,n+1)]
-tot = sum([j*(p**i) for i,j in terms])
-assert tot == m
+assert sum([j*(p**i) for i,j in terms]) == m
 a = (sum([j*(r**(i+1)) for i,j in terms]) + s) % p
 print a
 
